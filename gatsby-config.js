de8f5a6b054f32b0dedba21aff7c15a5ee11ccf1
unknown <chris.a.desilva@gmail.com>
2019-02-14 17:52:27 -0600

    Update background and theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#A2211A`,
+        theme_color: `#A2211A`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
